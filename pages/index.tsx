c902aa6ad71e92217b01bd9d14462a595c8cb70e
Janakan Chandrakumar <j8chandr@uwaterloo.ca>
2021-05-31 15:24:51 -0400

    Initial landing page mock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Code, Text, Link, VStack, Grid } from "@chakra-ui/react"
+import { Box, Code, Text, Link, VStack, Grid, Heading, Flex, Stack, Button, Image } from "@chakra-ui/react"
 import { Chakra } from "../Chakra"
 import { Layout } from "../components/Layout"
 import { Logo } from "../components/Logo"
@@ -11,6 +11,83 @@ interface IndexProps {
 const IndexPage = ({ cookies }: IndexProps) => (
   <Chakra cookies={cookies}>
     <Layout title="Home | LabConnect">
+     <Flex
+      align="center"
+      justify={{ base: "center", md: "space-around", xl: "space-between" }}
+      direction={{ base: "column-reverse", md: "row" }}
+      wrap="no-wrap"
+      minH="70vh"
+      px={8}
+      mb={16}
+    >
+      <Stack
+        spacing={4}
+        w={{ base: "80%", md: "40%" }}
+        align={["center", "center", "flex-start", "flex-start"]}
+      >
+        <Heading
+          as="h1"
+          size="xl"
+          fontWeight="bold"
+          color="primary.800"
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Welcome to LabConnect
+        </Heading>
+        <Heading
+          as="h2"
+          size="md"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Looking for research opportunities?
+        </Heading>
+        <Heading
+          as="h2"
+          size="sm"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={["center", "center", "left", "left"]}
+        >
+          We make finding labs, professors, and research jobs that match your interests easy.
+        </Heading>
+        <Link to="">
+          <Button
+            colorScheme="primary"
+            borderRadius="8px"
+            py="4"
+            px="4"
+            lineHeight="1"
+            size="md"
+          >
+            link
+          </Button>
+        </Link>
+        <Text
+          fontSize="xs"
+          mt={2}
+          textAlign="center"
+          color="primary.800"
+          opacity="0.6"
+        >
+          Made with ♥ in Ottawa + Montreal
+        </Text>
+      </Stack>
+      <Box 
+        w={{ base: "80%", sm: "60%", md: "50%" }} 
+        mb={{ base: 12, md: 0 }}>
+        {/* TODO: Make this change every X secs */}
+        <Image 
+          src="https://i.pinimg.com/originals/04/ef/5e/04ef5e1743f2123165f573616c533885.jpg" 
+          size="100%"
+          rounded="1rem" shadow="2xl" />
+      </Box>
+    </Flex>
     </Layout>
   </Chakra>
 )
