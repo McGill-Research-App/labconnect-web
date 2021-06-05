608b3b40b6ad095c5de0aac8387808e7acf57aa5
sodouglas <douglas.so@mail.mcgill.ca>
2021-06-05 08:37:49 -0400

    Split Prof faculties layout into 2 columns

--- pages/professors/index.tsx
+++ pages/professors/index.tsx
@@ -9,9 +9,10 @@ import {
   List,
   ListItem,
   Text,
+  SimpleGrid,
+  Box,
 } from "@chakra-ui/react";
 import { Layout } from "../../components/Layout";
-import { ProfessorSummary } from "../../components/ProfessorSummary";
 import { NextChakraLink } from "../../components/NextChakraLink";
 import { Chakra } from "../../Chakra";
 import data from "../../utils/prof-data.json";
@@ -28,120 +29,92 @@ const ProfessorsPage = ({ cookies }: ProfessorProps) => (
         Professors
       </Heading>
 
+      {/* Structured as: Faculty –> Department/School –> Professor list */}
       <Accordion allowToggle>
-        {facdata.map((item) => {
-          return (
-            <AccordionItem>
-              <AccordionButton>
-                <Heading my={4}>{item.faculty}</Heading>
-                <AccordionIcon />
-              </AccordionButton>
+        {facdata.map((item) => (
+          <AccordionItem>
+            <AccordionButton>
+              <Heading my={3}>{item.faculty}</Heading>
+              <AccordionIcon />
+            </AccordionButton>
 
-              <AccordionPanel>
-                <Accordion allowToggle>
-                  {item.departments.length > 0 && (
-                    <Heading as="h3" size="lg" color="gray" my={5}>
-                      Departments
-                    </Heading>
-                  )}
-                  {item.departments.map((dept) => (
-                    <AccordionItem>
-                      <AccordionButton>
-                        <Heading my={4} as="h3" size="md">
-                          {dept}
-                        </Heading>
-                        <AccordionIcon />
-                      </AccordionButton>
+            <AccordionPanel>
+              <SimpleGrid columns={2} spacing={4}>
+                {item.departments.length > 0 && (
+                  <Box>
+                    <Accordion allowToggle>
+                      <Heading as="h3" size="lg" color="gray" my={3}>
+                        Departments
+                      </Heading>
+                      {item.departments.map((dept) => (
+                        <AccordionItem>
+                          <AccordionButton>
+                            <Heading my={1} as="h3" size="md">
+                              {dept}
+                            </Heading>
+                            <AccordionIcon />
+                          </AccordionButton>
 
-                      {dept == "Pharmacology & Therapeutics" ? (
-                        <AccordionPanel>
-                          <List spacing={3} ml={2}>
-                            {data.map((professor) => (
-                              <ListItem>
-                                <NextChakraLink
-                                  href="/professors/[id]"
-                                  as={`/professors/${professor.name
-                                    .replace(/\s+/g, "-")
-                                    .toLowerCase()}`}
-                                >
-                                  <HStack spacing="3">
-                                    <Text color="gray">{professor.title}</Text>
-                                    <Text color="blackAlpha">
-                                      {professor.name}
-                                    </Text>
-                                  </HStack>
-                                </NextChakraLink>
-                              </ListItem>
-                            ))}
-                          </List>
-                        </AccordionPanel>
-                      ) : (
-                        <AccordionPanel>(Professor list here)</AccordionPanel>
-                      )}
-                    </AccordionItem>
-                  ))}
+                          {dept == "Pharmacology & Therapeutics" ? (
+                            <AccordionPanel>
+                              <List spacing={3} ml={2}>
+                                {data.map((professor) => (
+                                  <ListItem>
+                                    <NextChakraLink
+                                      href="/professors/[id]"
+                                      as={`/professors/${professor.name
+                                        .replace(/\s+/g, "-")
+                                        .toLowerCase()}`}
+                                    >
+                                      <HStack spacing="3">
+                                        <Text color="gray">
+                                          {professor.title}
+                                        </Text>
+                                        <Text color="blackAlpha">
+                                          {professor.name}
+                                        </Text>
+                                      </HStack>
+                                    </NextChakraLink>
+                                  </ListItem>
+                                ))}
+                              </List>
+                            </AccordionPanel>
+                          ) : (
+                            <AccordionPanel>
+                              (Professor list here)
+                            </AccordionPanel>
+                          )}
+                        </AccordionItem>
+                      ))}
+                    </Accordion>
+                  </Box>
+                )}
 
-                  {item.schools.length > 0 && (
-                    <Heading as="h3" size="lg" color="gray" my={5}>
-                      Schools
-                    </Heading>
-                  )}
-                  {item.schools.map((schl) => (
-                    <AccordionItem>
-                      <AccordionButton>
-                        <Heading my={4} as="h3" size="md">
-                          {schl}
-                        </Heading>
-                        <AccordionIcon />
-                      </AccordionButton>
-                      <AccordionPanel>(Professor list here)</AccordionPanel>
-                    </AccordionItem>
-                  ))}
-                </Accordion>
-              </AccordionPanel>
-            </AccordionItem>
-          );
-        })}
+                {item.schools.length > 0 && (
+                  <Box>
+                    <Accordion allowToggle>
+                      <Heading as="h3" size="lg" color="gray" my={3}>
+                        Schools
+                      </Heading>
+                      {item.schools.map((schl) => (
+                        <AccordionItem>
+                          <AccordionButton>
+                            <Heading my={1} as="h3" size="md">
+                              {schl}
+                            </Heading>
+                            <AccordionIcon />
+                          </AccordionButton>
+                          <AccordionPanel>(Professor list here)</AccordionPanel>
+                        </AccordionItem>
+                      ))}
+                    </Accordion>
+                  </Box>
+                )}
+              </SimpleGrid>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
       </Accordion>
-
-      {/* <AccordionItem>
-          <AccordionButton>
-            <Heading my={4}>Medicine and Health Sciences</Heading>
-            <AccordionIcon />
-          </AccordionButton>
-          <AccordionPanel>
-            <Accordion allowMultiple defaultIndex={[0]}>
-              <AccordionItem>
-                <AccordionButton>
-                  <Heading as="h3" size="md" my={4}>
-                    Pharmacology & Therapeutics
-                  </Heading>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel>
-                  <List spacing={3} ml={2}>
-                    {data.map((professor) => (
-                      <ListItem>
-                        <NextChakraLink
-                          href="/professors/[id]"
-                          as={`/professors/${professor.name
-                            .replace(/\s+/g, "-")
-                            .toLowerCase()}`}
-                        >
-                          <HStack spacing="3">
-                            <Text color="gray">{professor.title}</Text>
-                            <Text color="blackAlpha">{professor.name}</Text>
-                          </HStack>
-                          {/* <ProfessorSummary prof={professor} /> */}
-      {/* </NextChakraLink>
-                      </ListItem>
-                    ))}
-                  </List>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </AccordionPanel>
-        </AccordionItem> */}
     </Layout>
   </Chakra>
 );
